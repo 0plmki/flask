83873c1471980ed6ecfdc56987110af3dae7f34b
0plmki <134048959+0plmki@users.noreply.github.com>
2024-07-18 12:05:57 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 import os
+from flask import Flask, jsonify
 import boto3
 import psycopg2
-from flask import Flask, jsonify
+from dotenv import load_dotenv
 
 app = Flask(__name__)
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Load environment variables
 RDS_HOST = os.getenv('RDS_HOST')
@@ -27,6 +31,7 @@ def get_db_connection():
 # Initialize S3 client
 s3_client = boto3.client(
     's3',
+    region_name='ap-south-1',
     aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
     aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY')
 )
